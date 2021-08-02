e8c1f78972f6f73511ba7cfd764682b39dab0fb6
Satoshi Matsushima <>
2021-08-03 00:05:20 +0900

    CRUD function

--- src/TaskItem.tsx
+++ src/TaskItem.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import firebase from 'firebase/app';
+import { db } from './firebase';
 
 import { ListItem, TextField, Grid } from '@material-ui/core';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
@@ -10,12 +12,37 @@ interface PROPS {
 }
 
 const TaskItem: React.VFC<PROPS> = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const editTask = () => {
+    db.collection('tasks').doc(props.id).set({ title }, { merge: true });
+  };
+  const deleteTask = () => {
+    db.collection('tasks').doc(props.id).delete();
+  };
+
   return (
-    <div>
-      <ListItem>
-        <h2>{props.title}</h2>
-      </ListItem>
-    </div>
+    <ListItem>
+      <h2>{props.title}</h2>
+      <Grid container justify="flex-end">
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="Edit task"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTitle(e.target.value)
+          }
+        />
+      </Grid>
+      <button onClick={editTask}>
+        <EditIcon />
+      </button>
+      <button onClick={deleteTask}>
+        <DeleteOutlineIcon />
+      </button>
+    </ListItem>
   );
 };
 
